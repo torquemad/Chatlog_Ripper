713e0caca4af2de88fa575b5a68f9a2aa32ea7b5
Louis Moore <burntliketoast@gmail.com>
2015-06-07 15:28:45 +1000

    Added example of if statement to filter by specified url, users can uncomment this

--- slack.rb
+++ slack.rb
@@ -1,26 +1,35 @@
-# Put your own destination url
-logs = File.open("/Users/YourComputerName/Desktop/logs.txt")
+require 'pry'
+require 'uri'
 
-# This regex will try it's best to grab URL's from your chat log file
+# Put your own destination url
+logs = File.open("/Users/USERNAME/PATHTOLOGFILE/logs.txt")
+
+# This regex will try it's best to grab URL's from your chat log file.
+# Returns a collection
 regexp = /(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/
 
 # Where we will append urls
 arr = []
-
 # Get each line from document and filter out URL's, put in arr
 logs.each_line do |line|
 
   
-  str = line.scan(regexp)
+  regex_arr = line.scan(regexp)
   
-  if str.length >= 1
-    arr << str.first[0]
+  if regex_arr.length >= 1
+
+    # Uncomment if you want to specify a specific url
+    # if regex_arr.first[0].include? "gist.github.com"
+      arr << regex_arr.first[0]
+    # end
+
   end
+
 
 end
 
 # Make new document, loop through each arr element and put to file.
-File.open("/Users/TERMITE/Desktop/logs2.txt", "w+") do |f|
+File.open("/Users/USERNAME.PATHTOOUTPUTFILE/clean_logs.txt", "w+") do |f|
   arr.each { |element| f.puts(element) }
 end
 
